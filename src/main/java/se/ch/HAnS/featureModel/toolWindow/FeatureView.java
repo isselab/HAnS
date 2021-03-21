9a92c290f8cb21785f92d33575a41acff3301e90
Herman <herman.jansson.mail@gmail.com>
2021-03-21 13:43:18 +0100

    no idea what this is

--- src/main/java/se/ch/HAnS/featureModel/toolWindow/FeatureView.java
+++ src/main/java/se/ch/HAnS/featureModel/toolWindow/FeatureView.java
@@ -16,15 +16,22 @@ import se.ch.HAnS.featureModel.psi.impl.FeatureModelProjectNameImpl;
 
 import javax.swing.*;
 import javax.swing.tree.*;
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class FeatureView extends JPanel {
+public class FeatureView extends JPanel implements ActionListener {
 
     private Project project;
+    private DefaultTreeModel tree;
     private DefaultMutableTreeNode root;
+
+    private static String ADD_COMMAND = "add";
+    private static String REMOVE_COMMAND = "remove";
+    private static String CLEAR_COMMAND = "clear";
 
     public FeatureView(Project project) {
         super(new BorderLayout());
@@ -32,11 +39,56 @@ public class FeatureView extends JPanel {
 
         getFeatureNames();
 
-        DefaultTreeModel tree = new DefaultTreeModel(root);
+        JButton addButton = new JButton("Add");
+        addButton.setActionCommand(ADD_COMMAND);
+        addButton.addActionListener(this);
 
+        JButton removeButton = new JButton("Remove");
+        removeButton.setActionCommand(REMOVE_COMMAND);
+        removeButton.addActionListener(this);
+
+        JButton clearButton = new JButton("Clear");
+        clearButton.setActionCommand(CLEAR_COMMAND);
+        clearButton.addActionListener(this);
+
+        tree = new DefaultTreeModel(root);
         Tree left = new Tree(tree);
+        add(new JScrollPane(left), BorderLayout.CENTER);
 
-        add(new JScrollPane(left));
+        JPanel buttons = new JPanel(new GridLayout(0, 3));
+        buttons.add(addButton);
+        buttons.add(removeButton);
+        buttons.add(clearButton);
+        add(buttons, BorderLayout.SOUTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        if (ADD_COMMAND.equals(command)) {
+            // Add button clicked
+            addObject(null);
+        } else if (REMOVE_COMMAND.equals(command)) {
+            // Remove button clicked
+            removeCurrentNode();
+        } else if (CLEAR_COMMAND.equals(command)) {
+            // Clear button clicked.
+            clear();
+        }
+    }
+
+    public DefaultMutableTreeNode addObject(Object child) {
+        return null;
+    }
+
+    public void removeCurrentNode() {
+        return;
+    }
+
+    public void clear() {
+        getFeatureNames();
+        tree.reload();
     }
 
     private void getFeatureNames() {
@@ -53,7 +105,7 @@ public class FeatureView extends JPanel {
         if (f != null) {
             f.accept(new PsiRecursiveElementWalkingVisitor() {
                 DefaultMutableTreeNode current;
-                int indent = 1;
+                int indent = 0;
 
                 IndentedNode no;
 
@@ -68,6 +120,7 @@ public class FeatureView extends JPanel {
                     else if (element instanceof FeatureModelFeatureImpl) {
                         if (element.getPrevSibling().getText().length() > indent) {
                             indent = element.getPrevSibling().getText().length();
+
                             DefaultMutableTreeNode n = new DefaultMutableTreeNode(element.getText());
                             current.add(n);
                             current = n;
@@ -75,16 +128,36 @@ public class FeatureView extends JPanel {
                             no = new IndentedNode(no, element.getPrevSibling().getText().length());
                         }
                         else if (element.getPrevSibling().getText().length() == indent) {
-                            current.add(new DefaultMutableTreeNode(element.getText()));
+                            DefaultMutableTreeNode p = (DefaultMutableTreeNode)current.getParent();
+                            DefaultMutableTreeNode n = new DefaultMutableTreeNode(element.getText());
+                            p.add(n);
+                            current = n;
 
                             no = new IndentedNode(no.getParent(), element.getPrevSibling().getText().length());
                         }
                         else {
-                            int i = no.getParent().getIndent();
-                            if (element.getPrevSibling().getText().length() == i) {
-
-                            }
                             indent = element.getPrevSibling().getText().length();
+
+                            while (no.getIndent() > indent) {
+                                current = (DefaultMutableTreeNode)current.getParent();
+                                no = no.getParent();
+
+                                if (no.getIndent() == indent) {
+                                    DefaultMutableTreeNode p = (DefaultMutableTreeNode)current.getParent();
+                                    DefaultMutableTreeNode n = new DefaultMutableTreeNode(element.getText());
+                                    p.add(n);
+                                    current = n;
+
+                                    no = new IndentedNode(no.getParent(), element.getPrevSibling().getText().length());
+                                }
+                                else if (no.getIndent() < indent) {
+                                    DefaultMutableTreeNode n = new DefaultMutableTreeNode(element.getText());
+                                    current.add(n);
+                                    current = n;
+
+                                    no = new IndentedNode(no, element.getPrevSibling().getText().length());
+                                }
+                            }
                         }
                     }
                     super.visitElement(element);
@@ -93,9 +166,9 @@ public class FeatureView extends JPanel {
         }
     }
 
-    private class IndentedNode {
-        private IndentedNode parent;
-        private int indent;
+    private static class IndentedNode {
+        private final IndentedNode parent;
+        private final int indent;
 
         public IndentedNode(IndentedNode p, int i) {
             this.parent = p;
