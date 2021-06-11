2ed7af42671e7ef5a8aebfa39b83900f18d08e41
johmara <johan.t.martinson@gmail.com>
2021-06-11 17:16:35 +0200

    fixed license without brackets....

--- src/main/java/se/ch/HAnS/referencing/FeatureAnnotationNamedElement.java
+++ src/main/java/se/ch/HAnS/referencing/FeatureAnnotationNamedElement.java
@@ -1,5 +1,5 @@
 /*
-Copyright [2021] [Herman Jansson & Johan Martinson]
+Copyright 2021 Herman Jansson & Johan Martinson
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
