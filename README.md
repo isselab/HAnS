4f979e87a8465fca6f5d8b9c89c9a63fc0e717e7
Johan Martinson <JohanM@centiro.se>
2023-01-12 14:29:40 +0100

    Update for intelliJ version 2022.2.4

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 *`Version 1.0`*
 
 HAnS is a plugin that aims to help developers with annotating software. The annotations themselves are a proven help with
-locating features, so called feature location.
+locating features, so-called feature location.
 
 [Demo video](https://youtu.be/cx_-ZshHLgA)
 
@@ -18,14 +18,42 @@ HAnS-text supports:
 
 ### How to install? ###
 
-* HAnS text can easily be installed as a plugin for IntelliJ. To set it up you need IntelliJ
-  2021.1 or later installed and the HAnS 1.0 plugin downloaded.
+HAnS text can easily be installed as a plugin for IntelliJ. To set it up you need IntelliJ
+2021.1 or later installed and the HAnS 1.0 plugin downloaded.
+
+*Comming to the IntelliJ plugin marketplace shortly*
 
 ### Contribution guidelines ###
+You are free to contribute to this repository but configure 
 
-* At the moment we are not accepting any contributions. This will be set up shortly.
+#### Getting started
+ + Clone the repository.
+ + Start adding your code for your new feature or bugfix
+ + Create a new branch
+ + Build and Test
+ + make a pull request
+
+To build and test you new snippet run the gradle [runIde] task.
+
+Branches should be named like this: *`folder`*/*`featureName`*/*`contributor`*-*`description`*.
+
+Folder can be either *`Feature`* or *`Bug`*. Featurename should be one that is already in the featuremodel or some new featurename.
+Lastly contributor is your git name and the description should be a short description of the new feature or the bug.
+
+#### As a reviewer
+The repository have policies in place (similar to other Git repositories) where we have a team of members that are responsible for ensuring the code quality of the features before they can be merge to the main/master branch. 
+If you have experience with plugin development or want to help out by becoming a member of the review team, 
+please reach out and contact any member in the existing team. 
+
+Information on which people to contact/are currently are in the review team can be found here:
++ @Johan Martinson
++ @Herman Jansson
+
+#### Code Quality
+Given that the plugin will have a widespread use and most likely be used repeatedly, it is essential that the code is of quality. 
+For that reason we have policies in place where reviewers are required to review and accept the code before it is 
+accepted into the main branch.
 
 ### Who do I talk to? ###
-
 * @Johan Martinson or @Herman Jansson
 * Other community or team contact: Mukelabai Mukelabai 
--- src/main/java/se/ch/HAnS/featureView/FeatureViewFactory.java
+++ src/main/java/se/ch/HAnS/featureView/FeatureViewFactory.java
@@ -15,59 +15,53 @@ limitations under the License.
 */
 package se.ch.HAnS.featureView;
 
-import com.intellij.ide.structureView.impl.StructureViewFactoryImpl;
 import com.intellij.ide.structureView.newStructureView.StructureViewComponent;
-import com.intellij.ide.ui.customization.CustomizationUtil;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 import se.ch.HAnS.AnnotationIcons;
-import se.ch.HAnS.featureView.FeatureViewModel;
 
 import java.util.Collection;
+
+import static com.intellij.psi.PsiManager.getInstance;
+import static com.intellij.psi.search.FilenameIndex.getAllFilesByExt;
+import static com.intellij.psi.search.FilenameIndex.getVirtualFilesByName;
+import static com.intellij.psi.search.GlobalSearchScope.projectScope;
 
 public class FeatureViewFactory implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         toolWindow.setIcon(AnnotationIcons.FeatureModelIcon);
-        StructureViewFactoryImpl s = new StructureViewFactoryImpl(project);
-        FileEditor e = FileEditorManager.getInstance(project).getAllEditors()[0];
-        PsiFile f = findFeatureModel(project);
+        var fileEditor = FileEditorManager.getInstance(project).getAllEditors()[0];
+        var psiFile = findFeatureModel(project);
 
-        @NotNull StructureViewComponent tab = new StructureViewComponent(e, new FeatureViewModel(f), project, true);
+        @NotNull StructureViewComponent tab = new StructureViewComponent(fileEditor, new FeatureViewModel(psiFile), project, true);
 
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        ContentFactory contentFactory = ContentFactory.getInstance();
         Content content = contentFactory.createContent(tab, "", false);
         toolWindow.getContentManager().addContent(content);
 
     }
 
-    private PsiFile findFeatureModel(@NotNull Project project){
-        PsiFile[] allFilenames = FilenameIndex.getFilesByName(project, ".feature-model", GlobalSearchScope.projectScope(project));
-        PsiFile f = null;
-        if (allFilenames.length > 0) {
-            f = allFilenames[0];
-        }
-        else {
-            Collection<VirtualFile> c = FilenameIndex.getAllFilesByExt(project, "feature-model");
-            if (!c.isEmpty()) {
-                f = PsiManager.getInstance(project).findFile(c.iterator().next());
+    private PsiFile findFeatureModel(@NotNull Project project) {
+        var allFilenames = getVirtualFilesByName(".feature-model", projectScope(project));
+        PsiFile psiFile = null;
+        if (allFilenames.size() > 0) {
+            psiFile = getInstance(project).findFile(allFilenames.iterator().next());
+        } else {
+            Collection<VirtualFile> virtualFileCollection = getAllFilesByExt(project, "feature-model");
+            if (!virtualFileCollection.isEmpty()) {
+                psiFile = getInstance(project).findFile(virtualFileCollection.iterator().next());
             }
         }
 
-        return f;
+        return psiFile;
     }
-
 }
