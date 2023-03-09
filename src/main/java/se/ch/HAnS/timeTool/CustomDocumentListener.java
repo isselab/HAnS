2451aa85846e534deb85b6e6bf212ffae6ff54c4
FadiA <fadia@centiro.se>
2023-03-09 15:21:11 +0100

    Bug fixes

--- src/main/java/se/ch/HAnS/timeTool/CustomDocumentListener.java
+++ src/main/java/se/ch/HAnS/timeTool/CustomDocumentListener.java
@@ -5,9 +5,6 @@ import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
-import com.intellij.openapi.editor.event.EditorEventMulticaster;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.*;
 import com.intellij.psi.*;
@@ -18,7 +15,6 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
     private final LogWriter logWriter;
     private final CustomTimer timer;
     private boolean featureModelLogged = false;
-
     public CustomDocumentListener(Project project) {
         this.project = project;
         logWriter = new LogWriter(System.getProperty("user.home") + "/Desktop", "log.txt");
@@ -29,54 +25,56 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
             public void documentChanged(@NotNull DocumentEvent event) {
                 String text = event.getDocument().getText();
                 String fileName = event.getDocument().toString();
-
+                    // log when "line[]" annotation is edited
                 if (text.contains("&line[]")) {
-                    if (timer.canLog(5000)) {  // wait 5 seconds before logging again
-                        String timestamp = timer.getCurrentDate();
-
-                        logWriter.writeToLog(fileName + " edit a '&line[]' annotation text at " + timestamp + "\n");
-                        timer.updateLastLogged();
-                    }
-                } else if (text.contains("&begin[]") && text.contains("&end[]")) {
-                    if (timer.canLog(5000)) {  // wait 5 seconds before logging again
-                        String timestamp = timer.getCurrentDate();
-                        logWriter.writeToLog(fileName + " edit a '&begin[]' and '&end[]' annotation text at " + timestamp + "\n");
-                        timer.updateLastLogged();
-                    }
-                } else if (timer.canLog(5000)) {
                     String timestamp = timer.getCurrentDate();
-                    logWriter.writeToLog(fileName + " edit a code line at " +  timestamp + "\n");
+                    logWriter.writeToLog(fileName + " editing a &line[] at " +  timestamp + "\n");
                     timer.updateLastLogged();
+                    // log when "begin[]" and "end[]" annotations are edited
+                } else if (text.contains("&begin[]") && text.contains("&end[]")) {
+                    String timestamp = timer.getCurrentDate();
+                    logWriter.writeToLog(fileName + " editing a &begin[] and &end[] at " +  timestamp + "\n");
+                    timer.updateLastLogged();
+                    // editing a file
+                } else if (!text.matches("\\s*")) { // check if the added text is not just whitespace
+                    if (timer.canLog(5000)) {
+                        String timestamp = timer.getCurrentDate();
+                        logWriter.writeToLog(fileName + " editing a code line at " +  timestamp + "\n");
+                        timer.updateLastLogged();
+                    }
                 }
-                VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener() {
-                    @Override
-                    public void propertyChanged(VirtualFilePropertyEvent event) {
-                    }
+            }
+        });
 
-                    @Override
-                    public void contentsChanged(VirtualFileEvent event) {
-                        VirtualFile file = event.getFile();
-                        String fileName = file.getName();
-                        if (fileName.endsWith(".feature-model") && !featureModelLogged) {
-                            String timestamp = timer.getCurrentDate();
+        registerVirtualFileListener();
+    }
 
-                            logWriter.writeToLog(fileName + " was edited at " + timestamp + "\n");
-                            featureModelLogged = true;
+    private void registerVirtualFileListener() {
+        VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener() {
+            @Override
+            public void propertyChanged(VirtualFilePropertyEvent event) {
+            }
 
-                            // Schedule a task to reset featureModelLogged after 2 seconds
-                            ApplicationManager.getApplication().invokeLater(new Runnable() {
-                                @Override
-                                public void run() {
-                                    featureModelLogged = false;
-                                }
-                            }, ModalityState.NON_MODAL);
+            @Override
+            public void contentsChanged(VirtualFileEvent event) {
+                VirtualFile file = event.getFile();
+                String fileName = file.getName();
+                if (fileName.endsWith(".feature-model") && !featureModelLogged) {
+                    String timestamp = timer.getCurrentDate();
+
+                    logWriter.writeToLog(fileName + " was edited at " + timestamp + "\n");
+                    featureModelLogged = true;
+
+                    ApplicationManager.getApplication().invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            featureModelLogged = false;
                         }
-                    }
-                });
+                    }, ModalityState.NON_MODAL);
+                }
             }
         });
     }
-
     @Override
     public void beforeChildAddition(@NotNull PsiTreeChangeEvent event) {
 
@@ -149,20 +147,5 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void propertyChanged(@NotNull PsiTreeChangeEvent event) {
-        PsiElement element = event.getElement();
-
-        String timestamp = timer.getCurrentDate();
-        if(timer.canLog(5000)){
-            logWriter.writeToLog("PSI element changed at " + timestamp + "\n");
-
-            if (element instanceof PsiFile file) {
-                logWriter.writeToLog("File name: " + file.getName() + " was modified.\n");
-            } else if (element instanceof PsiDirectory directory) {
-                logWriter.writeToLog("Directory name: " + directory.getName() + " was modified.\n");
-            } else {
-                logWriter.writeToLog("Element name: " + element.toString() + " was modified.\n");
-            }
-            timer.updateLastLogged();
-        }
     }
 }
