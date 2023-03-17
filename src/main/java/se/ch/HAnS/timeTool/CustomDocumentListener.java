2aec89d65aac29a27efece087204eff9c52f4783
Civiliangem0 <fadd@live.se>
2023-03-18 00:06:50 +0100

    Changed so we use PsiFile when listening for editing and changes inside .feature-model, added so the logger detects whenever an annotation is deleted, and added some comments.

--- src/main/java/se/ch/HAnS/timeTool/CustomDocumentListener.java
+++ src/main/java/se/ch/HAnS/timeTool/CustomDocumentListener.java
@@ -1,7 +1,9 @@
 package se.ch.HAnS.timeTool;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
@@ -9,6 +11,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.*;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomDocumentListener implements PsiTreeChangeListener {
     private final Project project;
@@ -21,42 +27,55 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
         logWriter = new LogWriter(System.getProperty("user.home") + "/Desktop", "log.txt");
         timer = new CustomTimer();
 
+        // A DocumentListener to detect changes in documents
         EditorFactory.getInstance().getEventMulticaster().addDocumentListener(new DocumentListener() {
+
+            // This method is called before a change is made to the document
+            @Override
+            public void beforeDocumentChange(@NotNull DocumentEvent event) {
+                // Get the text that was deleted in the document change event
+                Document document = event.getDocument();
+                String deletedText = document.getText().substring(event.getOffset(), event.getOffset() + event.getOldLength());
+
+                // Check if the deleted text matches the annotation pattern, including possible leading whitespaces
+                Pattern pattern = Pattern.compile("^\\s*//\\s*&");
+                Matcher matcher = pattern.matcher(deletedText);
+                if (matcher.find()) {
+                    // Get the PsiFile associated with the document and retrieve its fileName
+                    PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+                    if (psiFile != null) {
+                        String fileName = psiFile.getName();
+                        logWriter.writeToJson(fileName, deletedText, timer.getCurrentDate());
+                        logWriter.writeToLog("A annotation in a file was removed at " + timer.getCurrentDate() + "\n");
+                    }
+                }
+            }
+
+            // This method is called after the document has been changed
             @Override
             public void documentChanged(@NotNull DocumentEvent event) {
             }
-        });
-
-        registerVirtualFileListener();
+        }, new EditorTracker(project));
     }
 
-    private void registerVirtualFileListener() {
-        VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener() {
-            @Override
-            public void propertyChanged(VirtualFilePropertyEvent event) {
-            }
-
-            @Override
-            public void contentsChanged(VirtualFileEvent event) {
-                VirtualFile file = event.getFile();
-                String fileName = file.getName();
-                if (fileName.endsWith(".feature-model") && !featureModelLogged) {
-
-                    logWriter.writeToJson(fileName, ".feature-model", timer.getCurrentDate());
-                    logWriter.writeToLog(fileName + " was edited at " + timer.getCurrentDate() + "\n");
-                    featureModelLogged = true;
-                    timer.updateLastLogged();
-
-                    ApplicationManager.getApplication().invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            featureModelLogged = false;
-                        }
-                    }, ModalityState.NON_MODAL);
+    // This method processes the file change events and logs specific changes based on the file type
+    private void processFileChange(PsiFile psiFile) {
+        String fileName = psiFile.getName();
+        // If the file is a .feature-model file, and it hasn't been logged yet, log the change
+        if (fileName.endsWith(".feature-model") && !featureModelLogged) {
+            logWriter.writeToJson(fileName, ".feature-model", timer.getCurrentDate());
+            logWriter.writeToLog(fileName + " was edited at " + timer.getCurrentDate() + "\n");
+            featureModelLogged = true;
+            timer.updateLastLogged();
+            ApplicationManager.getApplication().invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    featureModelLogged = false;
                 }
-            }
-        });
+            }, ModalityState.NON_MODAL);
+        }
     }
+
 
     @Override
     public void beforeChildAddition(@NotNull PsiTreeChangeEvent event) {
@@ -90,6 +109,7 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void childAdded(@NotNull PsiTreeChangeEvent event) {
+        processFileChange(Objects.requireNonNull(event.getFile()));
         PsiElement psiElement = event.getChild();
         String fileName = psiElement.getContainingFile().getName();
         if (psiElement instanceof PsiComment) {
@@ -108,6 +128,7 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void childRemoved(@NotNull PsiTreeChangeEvent event) {
+        processFileChange(Objects.requireNonNull(event.getFile()));
         PsiElement psiElement = event.getChild();
         String fileName = psiElement.getContainingFile().getName();
 
@@ -128,6 +149,7 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void childReplaced(@NotNull PsiTreeChangeEvent event) {
+        processFileChange(Objects.requireNonNull(event.getFile()));
         PsiElement oldChild = event.getOldChild();
         PsiElement newChild = event.getNewChild();
         String fileName = event.getFile().getName();
@@ -141,6 +163,8 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
             }
         }
     }
+
+    // This method checks if the given PsiComment object is an annotation comment (so if it starts with "// &")
     private boolean isAnnotationComment(PsiComment comment) {
         String text = comment.getText();
         return text.startsWith("// &");
@@ -148,7 +172,7 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void childrenChanged(@NotNull PsiTreeChangeEvent event) {
-
+        processFileChange(Objects.requireNonNull(event.getFile()));
     }
 
     @Override
@@ -158,5 +182,19 @@ public class CustomDocumentListener implements PsiTreeChangeListener {
 
     @Override
     public void propertyChanged(@NotNull PsiTreeChangeEvent event) {
+    }
+
+    // The EditorTracker class is used to track editor events and clean up resources when the project is disposed
+    static class EditorTracker implements Disposable {
+        private final Project project;
+
+        EditorTracker(Project project) {
+            this.project = project;
+        }
+
+        @Override
+        public void dispose() {
+            // Cleanup or unregister resources when the project is disposed
+        }
     }
 }
