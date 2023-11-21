18d0b242e503c050f4eaeb6a23cc4b97a83a388f
Kuzzi04 <95016273+Kuzzi04@users.noreply.github.com>
2023-11-21 17:10:27 +0100

    Created Feature Extension package Updated plugin.xml to get feature extension into other plugins Added version and id into plugin.xml, so other plugins can depend on it while testing

--- src/main/java/se/isselab/HAnS/featureExtension/FeatureExtensionEP.java
+++ src/main/java/se/isselab/HAnS/featureExtension/FeatureExtensionEP.java
@@ -1,13 +1,11 @@
 package se.isselab.HAnS.featureExtension;
 
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.extensions.PluginAware;
-import com.intellij.openapi.extensions.PluginDescriptor;
+
 import com.intellij.psi.PsiFile;
 import com.intellij.serviceContainer.LazyExtensionInstance;
 import com.intellij.util.xmlb.annotations.Attribute;
-import org.jetbrains.annotations.NotNull;
+
 import org.jetbrains.annotations.Nullable;
 
 public final class FeatureExtensionEP extends LazyExtensionInstance<FeatureExtension> {
