6f4a2300cdb6e92390e14df3cb99629555e08abe
Philipp Kusmierz <95016273+Kuzzi04@users.noreply.github.com>
2024-01-20 09:11:33 +0100

    Fixed feature highlighting and added doc-comments

--- src/main/java/se/isselab/HAnS/featureExposer/FeatureExposer.java
+++ src/main/java/se/isselab/HAnS/featureExposer/FeatureExposer.java
@@ -1,34 +1,52 @@
 package se.isselab.HAnS.featureExposer;
 
-import com.intellij.codeInsight.navigation.NavigationUtil;
+
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.ScrollType;
 import com.intellij.openapi.editor.markup.*;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.util.ui.JBUI;
+import com.intellij.util.PsiNavigateUtil;
+
 import se.isselab.HAnS.featureModel.FeatureModelUtil;
 import se.isselab.HAnS.featureModel.psi.FeatureModelFeature;
 
-import java.awt.*;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
+
 
 public class FeatureExposer {
+    /**
+     * Highlights a feature in the feature model of the project based on its LPQ.
+     * @param project Project
+     * @param featureLpq String
+     */
     public static void highlightFeatureInFeatureModel(Project project, String featureLpq) {
 
         List<FeatureModelFeature> selectedFeatures = ReadAction.compute(() -> FeatureModelUtil.findLPQ(project, featureLpq));
-        if(selectedFeatures.isEmpty())
+        if(selectedFeatures.isEmpty()){
             return;
-        ApplicationManager.getApplication().invokeLater(() -> NavigationUtil.openFileWithPsiElement(selectedFeatures.get(0), false, false));
+        }
+        ApplicationManager.getApplication().invokeLater(() -> PsiNavigateUtil.navigate(selectedFeatures.get(0)));
     }
+    /**
+     * Highlights a feature in the feature model of the project.
+     * @param project Project
+     * @param feature {@link FeatureModelFeature}
+     */
+    public static void highlightFeatureInFeatureModel(Project project, FeatureModelFeature feature) {
+        ApplicationManager.getApplication().invokeLater(() -> PsiNavigateUtil.navigate(feature));
+    }
+
+    /**
+     * Opens a file of the project in the editor
+     * @param project Project
+     * @param path String: Absolute path of the file
+     */
     public static void openFileInProject(Project project, String path){
         ApplicationManager.getApplication().invokeLater(() -> {
             String newPath = project.getBasePath() + path;
@@ -37,6 +55,13 @@ public class FeatureExposer {
             FileEditorManager.getInstance(project).openFile(open, false);
         });
     }
+    /**
+     * Opens a file of the project in the editor and highlights code block
+     * @param project Project
+     * @param path String: Absolute path of the file
+     * @param startline of the codeblock
+     * @param endline of the codeblock
+     */
     public static void openFileInProject(Project project, String path, int startline, int endline){
         ApplicationManager.getApplication().invokeLater(() -> {
             String newPath = project.getBasePath() + path;
