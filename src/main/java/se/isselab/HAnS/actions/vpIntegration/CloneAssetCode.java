c24830dda280854d866a3f14c1e7fea360f574a7
Ahmad Al Shihabi <ahmadalshihabi99@gmail.com>
2024-01-25 23:02:58 +0100

    paste psiMethods and psiClasses

--- src/main/java/se/isselab/HAnS/actions/vpIntegration/CloneAssetCode.java
+++ src/main/java/se/isselab/HAnS/actions/vpIntegration/CloneAssetCode.java
@@ -6,10 +6,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiMethod;
+import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,6 +17,8 @@ import java.util.List;
 
 public class CloneAssetCode extends AnAction {
     public static List<PsiElement> elementsInRange;
+    public static PsiMethod clonedMethod;
+    public static PsiClass clonedClass;
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
@@ -27,42 +26,59 @@ public class CloneAssetCode extends AnAction {
         PsiFile psiFile = PsiDocumentManager.getInstance(anActionEvent.getProject()).getPsiFile(editor.getDocument());
         if (psiFile == null) return;
         SelectionModel selectionModel = editor.getSelectionModel();
-        int caretOffset = editor.getCaretModel().getOffset();
-        PsiElement endElement = psiFile.findElementAt(caretOffset);
-        PsiMethod methodAtCaret = PsiTreeUtil.getParentOfType(endElement, PsiMethod.class);
-        System.out.println(methodAtCaret.getText());
-        getHighlightedBlock(selectionModel, psiFile);
-
+        resetClones();
+        if(selectionModel.hasSelection()){
+            getHighlightedBlock(selectionModel, psiFile);
+            return;
         }
+        int caretOffset = editor.getCaretModel().getOffset();
+        PsiElement element = psiFile.findElementAt(caretOffset);
+        PsiMethod methodAtCaret = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
+        if(methodAtCaret != null){
+            cloneMethod(methodAtCaret);
+        } else {
+            cloneClass(element);
+        }
+    }
+
+    private void resetClones() {
+        clonedClass = null;
+        clonedMethod = null;
+        elementsInRange = null;
+    }
+
+    private void cloneClass(PsiElement element) {
+        PsiClass classAtCaret = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        clonedClass = classAtCaret;
+    }
+
+    private void cloneMethod(PsiMethod methodAtCaret) {
+        clonedMethod = methodAtCaret;
+    }
 
     private void getHighlightedBlock(SelectionModel selectionModel, PsiFile psiFile){
-        if (selectionModel.hasSelection()) {
-            // Get start and end offsets of the selected text
-            int startOffset = selectionModel.getSelectionStart();
-            int endOffset = selectionModel.getSelectionEnd();
-
-            // Get the PSI elements corresponding to these offsets
-            PsiElement startElement = psiFile.findElementAt(startOffset);
-            PsiElement endElement = psiFile.findElementAt(endOffset);
-
-            // Create a list to hold all PsiElements within the range
-            List<PsiElement> elementsInRange = new ArrayList<>();
-
-            // Traverse from startElement to endElement and collect PsiElements
-            PsiElement currentElement = startElement;
-            while (currentElement != null && !currentElement.equals(endElement)) {
-                elementsInRange.add(currentElement);
-                currentElement = PsiTreeUtil.nextLeaf(currentElement);
-            }
-
-            // Add the endElement to the list if it's not null
-            if (endElement != null) {
-                elementsInRange.add(endElement);
-            }
-            if(startElement != null && endElement != null){
-                cloneBlock(elementsInRange);
-            }
+        // Get start and end offsets of the selected text
+        int startOffset = selectionModel.getSelectionStart();
+        int endOffset = selectionModel.getSelectionEnd();
+        // Get the PSI elements corresponding to these offsets
+        PsiElement startElement = psiFile.findElementAt(startOffset);
+        PsiElement endElement = psiFile.findElementAt(endOffset);
+        // Create a list to hold all PsiElements within the range
+        List<PsiElement> elementsInRange = new ArrayList<>();
+        // Traverse from startElement to endElement and collect PsiElements
+        PsiElement currentElement = startElement;
+        while (currentElement != null && !currentElement.equals(endElement)) {
+            elementsInRange.add(currentElement);
+            currentElement = PsiTreeUtil.nextLeaf(currentElement);
         }
+        // Add the endElement to the list if it's not null
+        if (endElement != null) {
+            elementsInRange.add(endElement);
+        }
+        if(startElement != null && endElement != null){
+            cloneBlock(elementsInRange);
+        }
+
     }
     private static void cloneBlock(List<PsiElement> elements) {
         elementsInRange = elements;
--- src/main/java/se/isselab/HAnS/actions/vpIntegration/PasteAssetCode.java
+++ src/main/java/se/isselab/HAnS/actions/vpIntegration/PasteAssetCode.java
@@ -6,9 +6,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -16,24 +14,39 @@ import java.util.List;
 public class PasteAssetCode extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        // Access the editor and project
         Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
         Project project = anActionEvent.getProject();
         if (editor == null || project == null) return;
-
-        // Get the PsiFile corresponding to the current editor
         PsiFile currentFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
         if (currentFile == null) return;
+        if(CloneAssetCode.elementsInRange != null){
+            pasteClonedElements(editor, project, currentFile);
+        } else if(CloneAssetCode.clonedClass != null){
+            pasteClonedClass(editor, project, currentFile);
+        } else if(CloneAssetCode.clonedMethod != null){
+            pasteClonedMethod(editor, project, currentFile);
+        }
 
-        pasteClonedElements(editor, project, currentFile);
+    }
+
+    private void pasteClonedMethod(Editor editor, Project project, PsiFile currentFile) {
+        PsiElement pasteTarget = getTargetPaste(editor, currentFile);
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            PsiMethod clonedMethodForInsertion = (PsiMethod) CloneAssetCode.clonedMethod.copy();
+            pasteTarget.getParent().addBefore(clonedMethodForInsertion, pasteTarget);
+        });
+    }
+
+    private void pasteClonedClass(Editor editor, Project project, PsiFile currentFile) {
+        PsiElement pasteTarget = getTargetPaste(editor, currentFile);
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            PsiClass clonedClassForInsertion = (PsiClass) CloneAssetCode.clonedClass.copy();
+            pasteTarget.getParent().addBefore(clonedClassForInsertion, pasteTarget);
+        });
     }
 
     private void pasteClonedElements(Editor editor, Project project, PsiFile currentFile) {
-        int caretOffset = editor.getCaretModel().getOffset();
-        PsiElement pasteTarget = currentFile.findElementAt(caretOffset);
-        if (pasteTarget == null) return;
-
-        // Get the list of elements to paste
+        PsiElement pasteTarget = getTargetPaste(editor, currentFile);
         List<PsiElement> elementsToPaste = CloneAssetCode.elementsInRange;
         if (elementsToPaste == null || elementsToPaste.isEmpty()) return;
 
@@ -44,5 +57,12 @@ public class PasteAssetCode extends AnAction {
                 pasteTarget.getParent().addBefore(clonedElement, pasteTarget);
             }
         });
+    }
+
+    private PsiElement getTargetPaste(Editor editor, PsiFile currentFile){
+        int caretOffset = editor.getCaretModel().getOffset();
+        PsiElement pasteTarget = currentFile.findElementAt(caretOffset);
+        if (pasteTarget == null) return null;
+        return pasteTarget;
     }
 }
