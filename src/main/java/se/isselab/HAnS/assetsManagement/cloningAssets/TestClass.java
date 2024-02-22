1088a218d0a73a96f57c6a9c4a7907d79bded45e
Ahmad Al Shihabi <ahmadalshihabi99@gmail.com>
2024-02-22 01:46:57 +0100

    Test method implemented

---
+++ src/main/java/se/isselab/HAnS/assetsManagement/cloningAssets/TestClass.java
@@ -0,0 +1,65 @@
+package se.isselab.HAnS.assetsManagement.cloningAssets;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import se.isselab.HAnS.actions.assetsManagement.CloneAsset;
+import se.isselab.HAnS.actions.assetsManagement.PasteClonedAsset;
+
+public class TestClass extends LightJavaCodeInsightFixtureTestCase {
+
+    public void testCloningFile() throws Exception {
+        final PsiFile sourceFile = myFixture.configureByText("SourceFile.java", "public class SourceFile {}");
+        assertNotNull(sourceFile);
+        final PsiDirectory sourceDirectory = sourceFile.getContainingDirectory();
+        assertNotNull(sourceDirectory);
+        DataContext dataContext = SimpleDataContext.builder()
+                .add(CommonDataKeys.PSI_ELEMENT, sourceFile)
+                .add(CommonDataKeys.PROJECT, getProject())
+                .build();
+        CloneAsset cloneAsset = new CloneAsset();
+        AnActionEvent e = AnActionEvent.createFromDataContext("ProjectViewPopup", null, dataContext);
+        cloneAsset.actionPerformed(e);
+
+        //pasting process
+        VirtualFile directoryVirtualFile = myFixture.getTempDirFixture().findOrCreateDir("src/targetDirectory");
+        PsiDirectory targetDirectory = myFixture.getPsiManager().findDirectory(directoryVirtualFile);
+        assertNotNull(targetDirectory);
+
+        DataContext pastingContext = SimpleDataContext.builder()
+                .add(CommonDataKeys.PSI_ELEMENT, targetDirectory)
+                .add(CommonDataKeys.PROJECT, getProject())
+                .build();
+        PasteClonedAsset pasteClonedAsset = new PasteClonedAsset();
+        AnActionEvent pasteEvent = AnActionEvent.createFromDataContext("ProjectViewPopup", null, pastingContext);
+        pasteClonedAsset.actionPerformed(pasteEvent);
+        PsiFile pastedFile = targetDirectory.findFile("SourceFile.java");
+        assertNotNull("The file was not pasted as expected", pastedFile);
+        assertEquals("The contents of the pasted file do not match", sourceFile.getText(), pastedFile.getText());
+
+        /*
+        PsiFile clonedFile = findClonedFile(sourceDirectory, "SourceFile.java"); // Implement this method based on your plugin's logic
+        assertNotNull("The file was not cloned as expected", clonedFile);
+        assertEquals("The contents of the cloned file do not match", sourceFile.getText(), clonedFile.getText());
+
+        CloneAsset cloneAsset = (CloneAsset) actionManager.getAction("clone");
+        final PsiFile sourceFile = myFixture.addFileToProject("src/graphics/Window.java", "public class Window extends JFrame {}");
+        final PsiDirectory sourceDirectory = sourceFile.getContainingDirectory();
+        assertNotNull(sourceFile);
+        assertNotNull(sourceDirectory);
+        assertNotNull(targetDirectory);
+        cloneAsset.testClone(sourceDirectory, targetDirectory);
+        PsiFile clonedFile = targetDirectory.findFile(sourceFile.getName());
+
+        assertNotNull("The file was not cloned as expected", clonedFile);
+        assertEquals("The contents of the cloned file do not match the source",
+                sourceFile.getText(), clonedFile.getText());
+         */
+    }
+}
