a21cf5faef0a7ae7830c61e4d7d6ed32d1794765
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-03-13 14:55:14 +0200

    comments

--- src/main/java/se/isselab/HAnS/metrics/NestingDepths.java
+++ src/main/java/se/isselab/HAnS/metrics/NestingDepths.java
@@ -3,13 +3,25 @@ import java.util.*;
 
 public class NestingDepths {
 
-    // NoAF: Calculate Number of File Annotations: total number of file
-    // annotations directly referencing the feature
+    /**
+     * Calculates the number of file annotations directly referencing the specified feature.
+     *
+     * @param tree The project structure tree containing file annotations.
+     * @param featureLPQ The feature's LPQ used to identify it.
+     * @return An Integer representing the total number of files annotated with the specified feature.
+     */
     public static Integer getNumberOfAnnotatedFiles(ProjectStructureTree tree, String featureLPQ) {
        int NoAF = countNumberOfAnnotatedFiles(tree, featureLPQ);
        return NoAF;
     }
 
+    /**
+     * Retrieves the number of features within the specified item (folder, file) in the project structure tree.
+     *
+     * @param tree The project structure tree containing the items with features.
+     * @param pathToItem The path to the item (folder, file) within the project structure tree.
+     * @return An Integer representing the number of features within the specified item. Returns -1 if the item is not found.
+     */
     public static Integer getNumberOfFeatures(ProjectStructureTree tree, String pathToItem) {
         // find corresponding node in the Project Tree
         ProjectStructureTree treeNode = findProjectTreeNode(tree, pathToItem);
@@ -20,19 +32,39 @@ public class NestingDepths {
         return features.size();
     }
 
-    // takes project tree and feature LPQ as parameter
-    // return max nesting depth of specific feature
+    /**
+     * Calculates the maximum nesting depth of a specific feature within the project tree.
+     *
+     * @param tree The project structure tree.
+     * @param feature The LPQ of the feature.
+     * @return An Optional Integer containing the maximum nesting depth of the feature, or empty if the feature is not found.
+     */
     public static Optional<Integer> getMaxNestingDepth(ProjectStructureTree tree, String feature) {
         return getFeatureDepths(tree, feature).stream().max(Integer::compareTo);
     }
 
+    /**
+     * Calculates the minimum nesting depth of a specific feature within the project tree.
+     *
+     * @param tree The project structure tree.
+     * @param feature The LPQ of the feature.
+     * @return An Optional Integer containing the minimum nesting depth of the feature, or empty if the feature is not found.
+     */
     public static Optional<Integer> getMinNestingDepth(ProjectStructureTree tree, String feature) {
         return getFeatureDepths(tree, feature).stream().min(Integer::compareTo);
     }
 
+    /**
+     * Calculates the average nesting depth of a specific feature within the project tree.
+     *
+     * @param tree The project structure tree.
+     * @param feature The LPQ of the feature.
+     * @return An Optional Integer containing the average nesting depth of the feature, or empty if the feature is not found.
+     */
     public static OptionalDouble getAvgNestingDepth(ProjectStructureTree tree, String feature) {
         return getFeatureDepths(tree, feature).stream().mapToDouble(Integer::doubleValue).average();
     }
+
     private static ArrayList<Integer> getFeatureDepths(ProjectStructureTree tree, String targetFeatureLPQ) {
         ArrayList<Integer> occurrences = new ArrayList<>();
         findAndStoreFeatureDepthsRecursive(tree, targetFeatureLPQ, occurrences);
