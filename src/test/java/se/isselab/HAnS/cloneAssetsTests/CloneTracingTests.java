eadbcb74eec361b040d4e06be31aed8ecb8884bc
Ahmad Al Shihabi <ahmadalshihabi99@gmail.com>
2024-04-06 02:30:21 +0200

    test

--- src/test/java/se/isselab/HAnS/cloneAssetsTests/CloneTracingTests.java
+++ src/test/java/se/isselab/HAnS/cloneAssetsTests/CloneTracingTests.java
@@ -1,44 +1,39 @@
 package se.isselab.HAnS.cloneAssetsTests;
 
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import org.junit.Before;
-import org.junit.Test;
-import se.isselab.HAnS.assetsManagement.cloneManagement.FeatureModelHandler;
-import se.isselab.HAnS.assetsManagement.cloneManagement.FeaturesAnnotationsExtractor;
 import se.isselab.HAnS.assetsManagement.cloneManagement.TracingHandler;
 
 import java.util.regex.Pattern;
 
 public class CloneTracingTests extends BasePlatformTestCase {
     TracingHandler tracingHandler;
-    FeatureModelHandler featureModelHandler;
-    FeaturesAnnotationsExtractor featuresAnnotationsExtractor;
 
-
-    @Before
-    public void setUp() {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
         tracingHandler = new TracingHandler();
-        featureModelHandler = new FeatureModelHandler();
-        featuresAnnotationsExtractor = new FeaturesAnnotationsExtractor();
     }
-
-    @Test
-    public void testStoreCloneTrace() throws Exception {
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/resources/cloneTestData";
     }
-
-    @Test
-    public void testCreateCopyFeatureTrace() {
-    }
-
-    @Test
-    public void testFindFeatureToFolderMappings() {
-    }
-
-    @Test
     public void testGetCurrentDateAndTime() {
+        TracingHandler tracingHandler = new TracingHandler();
         String dateTime = tracingHandler.getCurrentDateAndTime();
         String regex = "\\d{4}\\d{2}\\d{2}\\d{2}\\d{2}\\d{2}";
         Pattern pattern = Pattern.compile(regex);
         assertTrue("The date and time format should match yyyyMMddHHmmss",pattern.matcher(dateTime).matches());
     }
+    public void testGetTraceFilePath(){
+        String traceFile = TracingHandler.getTraceFilePath(myFixture.getProject());
+        assertNotNull("Trace file not found", traceFile);
+    }
+    public void testStoreCloneTrace(){
+        VirtualFile fileToCopy = myFixture.configureByFile("CloneFile.java").getVirtualFile();
+        tracingHandler.storeCloneTrace(myFixture.getProject(), myFixture.getProject().getName(), fileToCopy.getPath(), fileToCopy.getPath());
+        //VirtualFile traceFile = myFixture.getProject().getBaseDir().findChild(".trace-db");
+        //assertNotNull("Trace file not found", traceFile);
+    }
+
 }
---
+++ src/test/resources/cloneTestData/CloneFile.java
@@ -0,0 +1,2 @@
+public class cloneFile {
+}
