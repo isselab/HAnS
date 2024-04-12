ad66cd0e074e8c21469d1a6e2f74233aedcc037c
Tim Anakin Sülz <tim.suelz@ruhr-uni-bochum.de>
2024-04-12 17:48:38 +0200

    Configuration Page

---
+++ src/main/java/se/ch/HAnS/configurationPage/CheckboxBuilder.java
@@ -0,0 +1,101 @@
+/**
+ Copyright 2023 Johan Martinson & Herman Jansson
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ **/
+package se.ch.HAnS.configurationPage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CheckboxBuilder {
+
+        private JPanel panel;
+        private JCheckBox checkBox;
+
+        final int DISTANCE = 20;
+    /** Checkbox Builder builds a panel in the form of the base IntelliJ Layout with a checkbox and an optional  **/
+    public CheckboxBuilder(String checkboxText, String descriptionText) {
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout()); // Use GridBagLayout for more control
+
+        panel.setMaximumSize(new Dimension(1000, 55));
+
+
+
+        checkBox = new JCheckBox(checkboxText);
+
+        // Create GridBagConstraints for the checkbox
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; // Set the column position
+        gbc.gridy = 0; // Set the row position
+        gbc.anchor = GridBagConstraints.NORTHWEST;// Align to the left
+        gbc.weighty = 1;
+        gbc.weightx = 1;
+        gbc.insets = new Insets(5, DISTANCE+5, 5, 5); // Add some padding
+        panel.add(checkBox, gbc); // Add checkbox with constraints
+
+        GridBagConstraints gbclabel = new GridBagConstraints();
+        gbclabel.gridx = 0;
+        gbclabel.gridy = 1;
+        gbclabel.anchor = GridBagConstraints.NORTHWEST;
+        gbclabel.insets = new Insets(25, DISTANCE+28, 5, 5);
+
+
+        JLabel descriptionLabel = new JLabel(descriptionText);
+        descriptionLabel.setForeground(Color.GRAY);
+
+
+        gbc.gridy++; // Move to the next row
+        panel.add(checkBox, gbc);
+        panel.add(descriptionLabel,gbclabel);// Add description label with constraints
+    }
+
+    public CheckboxBuilder(String checkboxText) {
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout()); // Use GridBagLayout for more control
+
+        panel.setMaximumSize(new Dimension(1000, 30));
+
+
+
+        checkBox = new JCheckBox(checkboxText);
+
+        // Create GridBagConstraints for the checkbox
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; // Set the column position
+        gbc.gridy = 0; // Set the row position
+        gbc.anchor = GridBagConstraints.NORTHWEST;// Align to the left
+        gbc.weighty = 1;
+        gbc.weightx = 1;
+        gbc.insets = new Insets(5, DISTANCE+5, 5, 5); // Add some padding
+        panel.add(checkBox, gbc); // Add checkbox with constraints
+
+
+
+
+
+        // Create GridBagConstraints for the description label
+        gbc.gridy++; // Move to the next row
+        panel.add(checkBox, gbc);
+
+    }
+
+        public JPanel getPanel() {
+            return panel;
+        }
+
+        public JCheckBox getCheckbox() {
+            return checkBox;
+        }
+}
---
+++ src/main/java/se/ch/HAnS/configurationPage/ConfigurationPage.java
@@ -0,0 +1,151 @@
+/**
+ Copyright 2023 Johan Martinson & Herman Jansson
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ **/
+package se.ch.HAnS.configurationPage;
+
+import com.intellij.openapi.options.*;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.util.*;
+import com.intellij.ui.components.*;
+import org.jetbrains.annotations.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ConfigurationPage implements Configurable {
+    @Override
+    public @NlsContexts.ConfigurableName String getDisplayName() {
+        return "HanS Plugin Settings";
+    }
+
+    @Override
+    public @Nullable JComponent createComponent() {
+
+        // Creating Base Panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel pluginDescriptionLabel = new JLabel("Settings for the HanS Plugin");
+        pluginDescriptionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(pluginDescriptionLabel);
+
+        // Creating different Tabs
+        JBTabbedPane tabbedPane = new JBTabbedPane();
+
+        // General Settings Tab
+        JPanel generalSettingsPanel = new JPanel();
+        generalSettingsPanel.setLayout(new BoxLayout(generalSettingsPanel, BoxLayout.Y_AXIS));
+
+        tabbedPane.addTab("General Settings", generalSettingsPanel);
+        // Section Hide Annotations
+        SectionBuilder hideAnnotationsSection = new SectionBuilder("Hide Annotations"); // Builds the section Hide Annotations
+        generalSettingsPanel.add(hideAnnotationsSection.getSettingsPanel());// Builds the section Hide Annotations into the generalSettings Panel
+
+
+        CheckboxBuilder enableHideAnnotationsCheckbox = new CheckboxBuilder("Enable Hide Annotations", "This Enables the Hide Annotation functionality ");// Builds the Checkbox
+        generalSettingsPanel.add(enableHideAnnotationsCheckbox.getPanel()); // add the Checkbox in typical IntelliJ Style to the Panel
+
+        // Adding action listener to the checkbox
+        enableHideAnnotationsCheckbox.getCheckbox().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             // TODO Add Hide Annotations functionality
+            }
+        });
+
+        // Section Logging
+        SectionBuilder loggingSection = new SectionBuilder("Logging");
+        generalSettingsPanel.add(loggingSection.getSettingsPanel());
+
+
+        CheckboxBuilder enableLoggingCheckbox = new CheckboxBuilder("Enable Logging");
+        generalSettingsPanel.add(enableLoggingCheckbox.getPanel());
+
+        // Adding action listener to the checkbox
+        enableLoggingCheckbox.getCheckbox().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO Add logging functionality
+
+            }
+        });
+
+        generalSettingsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Clone Settings Tab
+        JPanel cloneSettingsPanel = new JPanel();
+        cloneSettingsPanel.setLayout(new BoxLayout(cloneSettingsPanel, BoxLayout.Y_AXIS));
+        // Section Clone Settings
+        SectionBuilder cloneSection = new SectionBuilder("Clone Settings");
+        cloneSettingsPanel.add(cloneSection.getSettingsPanel());
+
+        CheckboxBuilder enableCloningTraceCheckbox = new CheckboxBuilder("Enable Cloning Trace Tracking");
+        cloneSettingsPanel.add(enableCloningTraceCheckbox.getPanel());
+
+        enableCloningTraceCheckbox.getCheckbox().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO Clone Trace Tracking Functionality should be implemented here
+            }
+        });
+
+        CheckboxBuilder showCloneInfoCheckbox = new CheckboxBuilder("Show Clone Information");
+        cloneSettingsPanel.add(showCloneInfoCheckbox.getPanel());
+
+        showCloneInfoCheckbox.getCheckbox().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO Show Clone Information should be added here
+
+            }
+        });
+
+        CheckboxBuilder sourcesSegmentChangesCheckbox = new CheckboxBuilder("Notify on Sources Segment Changes", "Here is the description text");
+        cloneSettingsPanel.add(sourcesSegmentChangesCheckbox.getPanel());
+
+        sourcesSegmentChangesCheckbox.getCheckbox().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO add the sources Segment Change functionality
+
+            }
+        });
+
+        cloneSettingsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        tabbedPane.addTab("Clone Settings", cloneSettingsPanel);
+
+        panel.add(tabbedPane);
+
+        TitledBorder titledBorder = BorderFactory.createTitledBorder("HanS Feature Settings");
+        panel.setBorder(titledBorder);
+
+        return panel;
+    }
+
+
+    @Override
+    public boolean isModified() {
+
+        return true;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+}
+
