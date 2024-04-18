e1736738907d512d2555b106bc3d921441a6bb0d
Tim_S <120215955+TimAnakin@users.noreply.github.com>
2024-04-18 09:46:04 +0200

    Update CheckboxBuilder.java

--- src/main/java/se/isselab/HAnS/settingsPage/CheckboxBuilder.java
+++ src/main/java/se/isselab/HAnS/settingsPage/CheckboxBuilder.java
@@ -1,6 +1,6 @@
 /**
- Copyright 2023 Johan Martinson & Herman Jansson
-
+ Copyright 2024 Tim Sülz
+ 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
