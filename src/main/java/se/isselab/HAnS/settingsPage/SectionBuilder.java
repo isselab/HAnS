2a4eb441a09be0df1b95b9f8e67200a0e37786e3
Tim_S <120215955+TimAnakin@users.noreply.github.com>
2024-04-18 09:46:43 +0200

    Update SectionBuilder.java

--- src/main/java/se/isselab/HAnS/settingsPage/SectionBuilder.java
+++ src/main/java/se/isselab/HAnS/settingsPage/SectionBuilder.java
@@ -1,5 +1,5 @@
 /**
- Copyright 2023 Johan Martinson & Herman Jansson
+ Copyright 2024 Tim Sülz
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
