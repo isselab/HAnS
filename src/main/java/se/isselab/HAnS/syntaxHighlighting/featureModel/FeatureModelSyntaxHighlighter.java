40850e3bf013656c3b5eae78503ffd63fe2fc5de
Cindy Liang <lmxcin@protonmail.com>
2025-02-27 10:17:29 +0100

    deleted a test statement

--- src/main/java/se/isselab/HAnS/syntaxHighlighting/featureModel/FeatureModelSyntaxHighlighter.java
+++ src/main/java/se/isselab/HAnS/syntaxHighlighting/featureModel/FeatureModelSyntaxHighlighter.java
@@ -58,13 +58,13 @@ public class FeatureModelSyntaxHighlighter extends SyntaxHighlighterBase {
     public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
         // System.out.println("Highlighting token: " + tokenType); // Temporary debug
         if (tokenType.equals(FeatureModelTypes.XOR_TOKEN)) {
-            System.out.println("Highlighting token: " + tokenType);
+
             return XOR_KEYS;
         } else if (tokenType.equals(FeatureModelTypes.OR_TOKEN)) {
-            System.out.println("Highlighting token: " + tokenType);
+
             return OR_KEYS;
         } else if (tokenType.equals(FeatureModelTypes.OPTIONAL)) {
-            System.out.println("Highlighting token: " + tokenType);
+
             return OPTIONAL_KEYS;
         } else if (tokenType.equals(FeatureModelTypes.FEATURENAME)){
             return FEATURE_KEYS;
