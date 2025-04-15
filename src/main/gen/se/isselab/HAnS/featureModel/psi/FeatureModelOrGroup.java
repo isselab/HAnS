fdd7f009ce788351c11841fe9629c07cd810193c
Johan Martinson <johan.t.martinson@gmail.com>
2025-04-15 11:11:18 +0200

    Revert "Optionally, OR/XOR and cross-tree constrains added to the Feature Model language"
    This reverts commit 3e9d74105b962a49f2a75484c4967c569b10d0f3.

--- src/main/gen/se/isselab/HAnS/featureModel/psi/FeatureModelOrGroup.java
+++ src/main/gen/se/isselab/HAnS/featureModel/psi/FeatureModelOrGroup.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 
-public interface FeatureModelCrossConstrains extends PsiElement {
+public interface FeatureModelOrGroup extends PsiElement {
 
   @NotNull
-  List<FeatureModelCrossConstrains> getCrossConstrainsList();
+  List<FeatureModelFeature> getFeatureList();
 
 }
