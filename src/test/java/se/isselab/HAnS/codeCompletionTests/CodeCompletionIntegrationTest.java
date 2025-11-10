3b6b1cbd514aed5e8e9aa73bd04e656e7443b2a7
Johan Martinson <johan.t.martinson@gmail.com>
2025-11-10 15:55:27 +0100

    Feature Name Completion and Rider IDE Compatibility (#179)

---
+++ src/test/java/se/isselab/HAnS/codeCompletionTests/CodeCompletionIntegrationTest.java
@@ -0,0 +1,167 @@
+/*
+Copyright 2025 Johan Martinson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package se.isselab.HAnS.codeCompletionTests;
+
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+
+/**
+ * Integration tests for code completion in feature annotation files.
+ * Tests end-to-end code completion scenarios with actual files and features.
+ */
+public class CodeCompletionIntegrationTest extends BasePlatformTestCase {
+
+    public void testFileNameCompletionInFeatureToFileWithFiles() {
+        // Create test Java files in the project
+        myFixture.configureByText("Helper.java", "public class Helper {}");
+        myFixture.configureByText("Helper2.java", "public class Utils {}");
+
+        // Create feature model with features
+        myFixture.configureByText("test.feature-model",
+                """
+                Root
+                    CoreFeature
+                    CoreFeature2
+                """);
+
+        // Test completion in feature-to-file path field
+        myFixture.configureByText("test.feature-to-file",
+                "<caret>Helper\n");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Completion may return null in test environment - this is acceptable
+        // Just verify it doesn't crash
+        assertNotNull("Should provide file name completions", completions);
+    }
+
+    public void testFeatureNameCompletionInAnnotationFile() {
+        // Create feature model with known features
+        myFixture.configureByText("features.feature-model",
+                """
+                Root
+                    Authentication
+                    Authorization
+                    Logging
+                """);
+
+        // Test completion for feature names starting with 'A'
+        myFixture.configureByText("test.feature-to-file",
+                "src/Auth.java\nAuth<caret>");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Should provide feature name completions
+        assertNotNull("Should provide feature name completions", completions);
+    }
+
+    public void testCompletionWithMultipleFeatures() {
+        // Create feature model with multiple features
+        myFixture.configureByText("model.feature-model",
+                """
+                Root
+                    UserManagement
+                    UserAuthentication
+                    UserProfile""");
+
+        // Test completion filters by prefix
+        myFixture.configureByText("test.feature-to-file",
+                "src/User.java\nUser<caret>");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // All completions should start with "User"
+        if (completions != null) {
+            for (LookupElement completion : completions) {
+                assertTrue("All features should start with 'User'",
+                        completion.getLookupString().startsWith("User"));
+            }
+        }
+    }
+
+    public void testPathCompletionFiltersCorrectly() {
+        // Create both regular files and annotation files
+        myFixture.configureByText("DataService.java", "public class DataService {}");
+        myFixture.configureByText("config.properties", "test=value");
+        myFixture.configureByText("exclude.feature-to-file", "test.java\nFeature");
+
+        myFixture.configureByText("test.feature-to-file",
+                "<caret>DataService");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Annotation files should be excluded IF completions are provided
+        // Null is acceptable in test environment
+        if (completions != null) {
+            for (LookupElement completion : completions) {
+                String name = completion.getLookupString();
+                assertFalse("Should exclude .feature-to-file files",
+                        name.endsWith(".feature-to-file"));
+                assertFalse("Should exclude .feature-to-folder files",
+                        name.endsWith(".feature-to-folder"));
+                assertFalse("Should exclude .code-annotation files",
+                        name.endsWith(".code-annotation"));
+            }
+        }
+    }
+
+    public void testEmptyPathCompletion() {
+        // Test completion with empty path field
+        myFixture.configureByText("test.feature-to-file",
+                "<caret>\nFeature1\n");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Should not break with empty completion
+        assertNotNull("Should handle empty path field", completions);
+    }
+
+    public void testCompletionInFeatureToFolder() {
+        // Create feature model
+        myFixture.configureByText("features.feature-model",
+                "Root\n    ModuleA\n");
+
+        // Test completion in feature-to-folder file
+        myFixture.configureByText("test.feature-to-folder",
+                "src/\nModule<caret>");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Should work same as feature-to-file
+        assertNotNull("Should provide completions in feature-to-folder", completions);
+    }
+
+    public void testCompletionPatternMatching() {
+        myFixture.configureByText("features.feature-model",
+                """
+                Root
+                    PaymentGateway
+                    PaymentProcessor
+                """);
+
+        myFixture.configureByText("file.txt", "regular file");
+        myFixture.configureByText("service.java", "regular java file");
+
+        // Test that completions work correctly
+        myFixture.configureByText("test.feature-to-file",
+                "fi<caret>le.txt\nPayment<caret>\n");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        assertNotNull("Pattern matching should work", completions);
+    }
+}
+
---
+++ src/test/java/se/isselab/HAnS/codeCompletionTests/FileCompletionContributorTest.java
@@ -0,0 +1,136 @@
+/*
+Copyright 2025 Johan Martinson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package se.isselab.HAnS.codeCompletionTests;
+
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import se.isselab.HAnS.codeCompletion.FileCompletionContributor;
+
+/**
+ * Tests for FileCompletionContributor to ensure completion providers are properly registered.
+ * Validates that both file name and feature name completion providers work together.
+ */
+public class FileCompletionContributorTest extends BasePlatformTestCase {
+
+    public void testFileCompletionContributorCanBeInstantiated() {
+        FileCompletionContributor contributor = new FileCompletionContributor();
+        assertNotNull("FileCompletionContributor should be instantiable", contributor);
+    }
+
+    public void testFileNameProviderRegisteredForPathField() {
+        // When completing in path field (non-feature-name STRING)
+        // the FileNameCompletionProvider should be active
+        myFixture.configureByText("test.feature-to-file",
+                "src<caret>/File.java\nMyFeature\n");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Should have file completions from the directory
+        assertNotNull("File completion should be available for path field", completions);
+    }
+
+    public void testFeatureNameProviderRegisteredForFeatureField() {
+        // When completing in feature field (feature-name STRING)
+        // the FeatureNameCompletionProvider should be active
+        myFixture.configureByText("test.feature-model",
+                """
+                Root
+                    AuthModule
+                """);
+
+        myFixture.configureByText("test.feature-to-file",
+                "src/Test.java\nAu<caret>");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Should have feature name completions
+        assertNotNull("Feature name completion should be available for feature field", completions);
+    }
+
+    public void testBothProvidersWork() {
+        // Create a test project with both files and features
+        myFixture.configureByText("test.feature-model",
+                """
+                Root
+                    TestFeature
+                """);
+
+        myFixture.configureByText("TestFile.java", "public class TestFile {}");
+
+        myFixture.configureByText("test.feature-to-file",
+                "<caret>TestFile");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Just verify completion invocation doesn't crash
+        assertNotNull("File name completions should work", completions);
+
+        myFixture.configureByText("test.feature-to-file",
+                "TestFile.java<caret>\nTestFea");
+
+        completions = myFixture.completeBasic();
+        assertNotNull("Feature name completions should work", completions);
+    }
+
+    public void testCompletionInFeatureToFolder() {
+        myFixture.configureByText("test.feature-model",
+                """
+                Root
+                    FolderFeature
+                """);
+
+        myFixture.configureByText("test.feature-to-folder",
+                "src/\nFold<caret>");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Feature name completion should work in feature-to-folder files too
+        assertNotNull("Completion should work in feature-to-folder files", completions);
+    }
+
+    public void testFileCompletionExcludesAnnotationFiles() {
+        // The FileNameCompletionProvider should exclude .feature-to-file
+        // and .feature-to-folder files
+        myFixture.configureByText("annotation.feature-to-file", "test.java\nFeature");
+        myFixture.configureByText("annotation2.feature-to-folder", "src/\nFeature");
+        myFixture.configureByText("regular.java", "public class Test {}");
+        myFixture.configureByText("regular.txt", "hello world");
+
+        myFixture.configureByText("test.feature-to-file",
+                "<caret>regular");
+
+        LookupElement[] completions = myFixture.completeBasic();
+
+        // Completions may or may not be provided, but if they are,
+        // they should exclude annotation files
+        // Null is acceptable in test environment
+        if (completions != null) {
+            for (LookupElement element : completions) {
+                String name = element.getLookupString();
+                assertFalse("Should not include .feature-to-file files",
+                        name.endsWith(".feature-to-file"));
+                assertFalse("Should not include .feature-to-folder files",
+                        name.endsWith(".feature-to-folder"));
+            }
+        }
+    }
+
+    public void testContributorConfigurationIsValid() {
+        FileCompletionContributor contributor = new FileCompletionContributor();
+        assertNotNull("FileCompletionContributor instance should not be null", contributor);
+    }
+}
+
